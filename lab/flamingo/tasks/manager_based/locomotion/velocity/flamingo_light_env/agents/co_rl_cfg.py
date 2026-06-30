cd6e251ca528e23c2b3d0e442f99f2cd39c5fe35
lch <orothy579@gmail.com>
2026-06-30 14:57:15 +0900

    Induce to feet off from the ground

--- lab/flamingo/tasks/manager_based/locomotion/velocity/flamingo_light_env/agents/co_rl_cfg.py
+++ lab/flamingo/tasks/manager_based/locomotion/velocity/flamingo_light_env/agents/co_rl_cfg.py
@@ -94,7 +94,7 @@ class FlamingoLightRoughPPORunnerCfg_StairJump(FlamingoPPORunnerCfg):
     def __post_init__(self):
         super().__post_init__()
 
-        self.max_iterations = 5000
+        self.max_iterations = 3000
         self.experiment_name = "Flamingo_Light_Rough_Stair_Jump"
         self.policy.actor_hidden_dims = [512, 256, 128]
         self.policy.critic_hidden_dims = [512, 256, 128]
